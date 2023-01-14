a82ef73a8fbab42c31c14b4e602de5d8528fbda5
Justus <justus.onyancha@moringaschool.com>
2023-01-15 00:11:55 +0300

    add porps, events and spread operator in event driven programming

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
 
 
-export default 
+export default function App(){
+
+  // const handleClick = () => {
+  //   console.log("clicked now");
+  // }
+
+  return (
+  <MyButton onClick= {handleClick}>
+  Click me
+  </MyButton>
+  );
+  
+}
+
+function handleClick(event){
+  console.log(event.nativeEvent);
+}
+
+function MyButton(props) {
+  return (
+    <button 
+    {...props}
+    
+    style={{
+      color: 'red'
+    }}>
+      {props.children}
+    </button>
+  ); 
+}
